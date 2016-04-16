1c2047ab44d1234bc7a6670c5ce041039355a178
Avika Narula <an1520@nyu.edu>
2016-04-16 18:58:47 -0400

    updated index.html

--- app/app.js
+++ app/app.js
@@ -4,7 +4,10 @@ var people = [];
 var stupid = [{"woman": "Beyonce" , "quote": "Who runs the world? Girls."},
                 {"woman": "Ginger Rogers" , "quote": "There is nothing a man can do, that I can not do better and in heels."},
                 {"woman": "Roseanne Barr", "quote": "The thing women have yet to learn is nobody gives you power. You just take it."},
-                {"woman" : "Charlotte Whitton", "quote" : "Whatever women do they must do twice as well as men to be thought half as good. Luckily, this is not difficult."}];
+                {"woman" : "Charlotte Whitton", "quote" : "Whatever women do they must do twice as well as men to be thought half as good. Luckily, this is not difficult."}, 
+                {"woman" : "Mary Harris Jones"}, "quote" : "I am not a humanitarian, I am a hell-raiser."}, 
+                {"woman" : "Gloria Steinem"} , "quote" : "A woman without a man is like a fish without a bicycle."}];
+
 
 
 var rand = Math.floor(Math.random() * stupid.length);
