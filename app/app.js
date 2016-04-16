5c104e6b6cef1c9f00325777b7920a31c45870d1
Avika Narula <an1520@nyu.edu>
2016-04-16 19:17:53 -0400

    pleaseee work

--- app/app.js
+++ app/app.js
@@ -5,10 +5,15 @@ var stupid = [{"woman": "Beyonce" , "quote": "Who runs the world? Girls."},
                 {"woman": "Ginger Rogers" , "quote": "There is nothing a man can do, that I can not do better and in heels."},
                 {"woman": "Roseanne Barr", "quote": "The thing women have yet to learn is nobody gives you power. You just take it."},
                 {"woman" : "Charlotte Whitton", "quote" : "Whatever women do they must do twice as well as men to be thought half as good. Luckily, this is not difficult."}, 
-                {"woman" : "Mary Harris Jones"}, "quote" : "I am not a humanitarian, I am a hell-raiser."}, 
-                {"woman" : "Gloria Steinem"} , "quote" : "A woman without a man is like a fish without a bicycle."}];
-
-
+                {"woman" : "Mary Harris Jones", "quote" : "I am not a humanitarian, I am a hell-raiser."}, 
+                {"woman" : "Gloria Steinem" , "quote" : "A woman without a man is like a fish without a bicycle."}, 
+                {"woman" : "Jane Goodall", "quote" : "My family has very strong women. My mother never laughed at my dream of Africa, even though everyone else did because we did not have any money, because Africa was the &rsquodark continent&rsquo, and because I was a girl." } ,
+                {"woman" : "Gloria Allred", "quote" : "There are some people who still feel threatened by strong women. That is their problem. It is not mine."} ,
+                {"woman" : "Simone de Beauvoir" , "quote": "I am too intelligent, too demanding, and too resourceful for anyone to be able to take charge of me entirely. No one knows me or loves me completely. I have only myself."} ,
+                {"woman" : "Margaret Sanger" , "quote" : "No woman can call herself free who does not control her own body."}, 
+                {"woman" : "Bell Hooks" , "quote" : "If any female feels she needs anything beyond herself to legitimate and validate her existence, she is already giving away her power to be self-defining, her agency."}, 
+                {"woman" : "Dolly Parton" , "quote" : "I am not going to limit myself just because people won&rsquot accept the fact that I can do something else."}, 
+                {"woman" : "Louisa May Alcott" , "quote" : "The emerging woman will be strong-minded, strong-hearted, strong-souled, and strong-bodied…strength and beauty must go together."}];
 
 var rand = Math.floor(Math.random() * stupid.length);
 console.log(rand); 
