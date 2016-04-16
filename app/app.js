dc38041bac46968e4024001b3007fd8c6b490ef8
hannavas <eleven.beyond@gmail.com>
2016-04-16 18:34:15 -0400

    gah

--- app/app.js
+++ app/app.js
@@ -1,4 +1,8 @@
 // will hold people objects
 var people = [];
-var responseObject = JSON.parse("clean_quotes.txt");
-console.log(responseObject);
+
+var stupid = [{"woman": "Beyoncé" , "quote": "Who runs the world? Girls."},
+                {"woman": "Ginger Rogers" , "quote": "There’s nothing a man can do, that I can’t do better and in heels."},
+                {"woman": "Roseanne Barr", "quote": "The thing women have yet to learn is nobody gives you power. You just take it."},
+                {"woman" : "Charlotte Whitton", "quote" : "Whatever women do they must do twice as well as men to be thought half as good. Luckily, this is not difficult."}];
+console.log(stupid);
